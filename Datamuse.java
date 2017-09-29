5f1664d4b232efe701e69277ffce654c875ec748
Mohammad Moustafa <mohammad.m@live.ca>
2017-09-29 11:46:40 -0400

    Update Datamuse.java

--- Datamuse.java
+++ Datamuse.java
@@ -4,12 +4,52 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.google.gson.Gson;
 
 public class Datamuse {
-	 
+	
+	private String url = "";
+	
     public Datamuse(){}
+    
+    public Datamuse QueryBuilder() {
+    	this.url = "http://api.datamuse.com/words?";
+    	return this;
+    }
+    
+    public Datamuse meansLike(String word) {
+    	word = word.replaceAll(" ", "+");
+    	if (this.url.equals("http://api.datamuse.com/words?")) {
+    		this.url += "ml=" + word;
+    	} else {
+    		this.url += "&ml=" + word;
+    	}
+    	System.out.println(this.url);
+    	return this;
+    }
+    
+    public Datamuse max(int limit) {
+    	if (this.url.equals("http://api.datamuse.com/words?")) {
+    		this.url += "max=" + limit;
+    	} else {
+    		this.url += "&max=" + limit;
+    	}
+    	System.out.println(this.url);
+    	return this;
+    }
+    
+    public DatamuseObject[] query() {
+    	String pattern = "(http://api.datamuse.com/words\\?max=)[0-9]+";
+    	Pattern r = Pattern.compile(pattern);
+    	Matcher m = r.matcher(this.url);
+    	if (m.find()) {
+    		System.out.println("'max' is the only endpoint in the builder. Will return an empty array.");
+    	}
+    	return parseJson(url);
+    }
 
     /**
      * Returns words that mean like the input, with a max number of words.
@@ -36,8 +76,9 @@ public class Datamuse {
 
             return buffer.toString();
         } finally {
-            if (reader != null)
+            if (reader != null) {
                 reader.close();
+            }
         }
     }
     
